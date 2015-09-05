f50015dc51486698f3a1287729fab986f2aff846
Jon Zobrist <jon@jonzobrist.com>
2015-09-05 13:14:26 -0700

    Adding Fizzbuzz python example

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/bin/env python
+
+for i in xrange(100):
+    output = "{} ".format(i)
+    if i % 3 == 0:
+        output = output + "Fizz"
+    if i % 5 == 0:
+        output = output + "Buzz"
+    print output
